497ccfab408cb4f11bd83088c98f3fd7438367ea
Junseong Park <junseongpark.dev@gmail.com>
2021-06-20 15:53:31 +0900

    Add image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "Junseong Park",
+    title: 'Junseong Park',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import {StaticImage} from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       I'm making this by following the Gatsby tutorial.
+      <StaticImage src="https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg" alt="A puppy" />
+      <StaticImage src="../images/puppy-1903313_1920.jpg" alt="A puppy" />
     </Layout>
   );
 };
