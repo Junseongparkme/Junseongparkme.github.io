f6a0090b39d9cc2f280dc17814907d76a57fc99f
Junseong Park <junseongpark.dev@gmail.com>
2021-06-20 21:03:35 +0900

    Use GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: 'Junseong Park',
+    title: 'Junseong Park WoW',
   },
   plugins: [
     'gatsby-plugin-gatsby-cloud',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
   ],
 };
