dd1f16afac4d62878954c8018a4d3953fa142f22
Junseong Park <junseongpark.dev@gmail.com>
2021-06-27 00:02:25 +0900

    Finish Simple GraphQL Test

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,31 +1,27 @@
 import * as React from 'react';
-import {graphql} from 'gatsby'
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-const BlogPage = ({data}) => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Post">
       <ul>
-        {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
-          ))
-        }
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
       </ul>
     </Layout>
   );
 };
 
 export const query = graphql`
-query {
-  allFile {
-    nodes {
-      name
+  query {
+    allFile {
+      nodes {
+        name
+      }
     }
   }
-}
-`
+`;
 
 export default BlogPage;
